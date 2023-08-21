acf331126319e3d843ce00209b0edfaa6bde79a4
uncenter <47499684+uncenter@users.noreply.github.com>
2023-08-21 09:35:19 -0400

    perf: self-host fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/assets');
 	eleventyConfig.addPassthroughCopy('src/scripts');
+	eleventyConfig.addPassthroughCopy('src/fonts');
 
 	return {
 		dir: { input: 'src', output: 'dist', data: '_data' },
